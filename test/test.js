324c0266aa1108428871b58ceef00eebb9d7f4a2
Tony Narlock <tony@git-pull.com>
2021-03-24 10:03:27 -0500

    Tests: Add tests for passing prettier options to import-sort, snapshots, etc

--- test/test.js
+++ test/test.js
@@ -30,3 +30,42 @@ test('another test', t => {
   const output = prettier.format(input, options)
   t.snapshot(output)
 })
+
+test('pass style: eslint', t => {
+  const options = {
+    filepath: 'general.js',
+    parser: 'babel',
+    plugins: ['.'],
+    importSortStyle: 'eslint'
+  }
+
+  const input = readFileSync(join(__dirname, './fixtures/general.js'), 'utf-8')
+  const output = prettier.format(input, options)
+  t.snapshot(output)
+})
+
+test('pass style: module', t => {
+  const options = {
+    filepath: 'general.js',
+    parser: 'babel',
+    plugins: ['.'],
+    importSortStyle: 'module'
+  }
+
+  const input = readFileSync(join(__dirname, './fixtures/general.js'), 'utf-8')
+  const output = prettier.format(input, options)
+  t.snapshot(output)
+})
+
+test('pass style: renke', t => {
+  const options = {
+    filepath: 'general.js',
+    parser: 'babel',
+    plugins: ['.'],
+    importSortStyle: 'renke'
+  }
+
+  const input = readFileSync(join(__dirname, './fixtures/general.js'), 'utf-8')
+  const output = prettier.format(input, options)
+  t.snapshot(output)
+})
